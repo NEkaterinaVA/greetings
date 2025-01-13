69a4ab609ee8cfa163ff7547bffe3cc6c958ad52
NEkaterinaVA <kotenokfine@mail.ru>
2025-01-14 02:28:18 +0300

    add return error

--- greetings.go
+++ greetings.go
@@ -3,8 +3,8 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
