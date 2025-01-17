f556714a78ae27daf186a2b6c0d3edaff041cf47
NEkaterinaVA <kotenokfine@mail.ru>
2025-01-17 19:06:41 +0300

    relocate rand function to hello package

--- greetings.go
+++ greetings.go
@@ -3,23 +3,22 @@ package greetings
 import (
     "errors"
     "fmt"
-    "math/rand"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
+func Hello(name string, randNum int) (string, error) {
     if name == "" {
         return "", errors.New("empty name")
     }
-    message := fmt.Sprintf(randomFormat(), name)
+    message := fmt.Sprintf(randomFormat(randNum), name)
     return message, nil
 }
 
-func randomFormat() string {
+func randomFormat(randNum int) string {
     formats := []string{
         "Hi, %v. Welcome!",
         "Great to see you, %v!",
         "Hail, %v! Well met!",
     }
-    return formats[rand.Intn(3)]
+    return formats[randNum]
 }
